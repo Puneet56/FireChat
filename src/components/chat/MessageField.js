3c69b0b3b7b68a8547ea633646488b306170eb3d
puneet2508 <puneetdhiman56@gmail.com>
2021-09-22 13:42:43 +0530

    removed wrong imports

--- src/components/chat/MessageField.js
+++ src/components/chat/MessageField.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useData } from '../../context/DataContext';
 
